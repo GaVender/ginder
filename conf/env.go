a3fdbda3c85f6324bc37c56cf585900b75e11c67
GaVender <lin1619@163.com>
2018-10-16 17:34:16 +0800

    add mongo usage

--- conf/env.go
+++ conf/env.go
@@ -9,24 +9,31 @@ import (
 	log4 "github.com/jeanphorn/log4go"
 	"strconv"
 	"gopkg.in/redis.v5"
+	"gopkg.in/mgo.v2"
+	"time"
 )
 
-var mysql_master_host string
-var mysql_slave_host  string
-var mysql_port 	      string
-var mysql_username    string
-var mysql_password	  string
-var mysql_db		  string
-var redis_master_host string
-var redis_slave_host  string
-var redis_port		  string
-var redis_password	  string
-var redis_db		  int
-var redis_pool_size   int
-var log_for_error	  string		// 放置非业务代码的错误的log
-var log_for_logic	  string		// 放置代码逻辑错误、运行参数、结果等的log
-var error_logger	  log4.Logger
-var logic_logger	  log4.Logger
+var mysql_master_host 		string
+var mysql_slave_host  		string
+var mysql_port 	      		string
+var mysql_username    		string
+var mysql_password	  		string
+var mysql_db		  		string
+var redis_master_host 		string
+var redis_slave_host  		string
+var redis_port		  		string
+var redis_password	  		string
+var redis_db		  		int
+var redis_pool_size   		int
+var mongo_host 				[]string
+var mongo_port		  		string
+var mongo_timeout			time.Duration
+var mongo_pool_limit		int
+var mongo_session    		*mgo.Session
+var log_for_error	  		string		// 放置非业务代码的错误的log路径
+var log_for_logic	  		string		// 放置代码逻辑错误、运行参数、结果等的log路径
+var error_logger	  		log4.Logger
+var logic_logger	  		log4.Logger
 
 func init() {
 	log_for_error = "/home/wwwlogs/php/ginder/php.log"
@@ -50,6 +57,10 @@ func DevStart() {
 	redis_password = ""
 	redis_db = 0
 	redis_pool_size = 10
+	mongo_host = []string{"127.0.0.1"}
+	mongo_port = "27017"
+	mongo_timeout = time.Second * 10
+	mongo_pool_limit = 10
 }
 
 func ProStart() {
@@ -65,6 +76,10 @@ func ProStart() {
 	redis_password = strings.TrimSpace(os.Getenv("redis_password"))
 	redis_db, _ = strconv.Atoi(os.Getenv("redis_db"))
 	redis_pool_size, _ = strconv.Atoi(os.Getenv("redis_pool_size"))
+	mongo_host = []string{"127.0.0.1"}
+	mongo_port = strings.TrimSpace(os.Getenv("MONGO_PORT"))
+	mongo_timeout = time.Second * 2
+	mongo_pool_limit, _ = strconv.Atoi(os.Getenv("MONGO_POOL_LIMIT"))
 }
 
 func SqlMasterDb() *sqlx.DB {
@@ -74,6 +89,7 @@ func SqlMasterDb() *sqlx.DB {
 
 	if err != nil {
 		LoggerError().Error("mysql connect error: %s", err.Error())
+		panic("mysql connect error")
 	}
 
 	return db
@@ -86,6 +102,7 @@ func SqlSlaveDb() *sqlx.DB {
 
 	if err != nil {
 		LoggerError().Error("mysql connect error: %s", err.Error())
+		panic("mysql connect error")
 	}
 
 	return db
@@ -126,4 +143,33 @@ func LoggerError() log4.Logger {
 
 func LoggerLogic() log4.Logger {
 	return logic_logger
+}
+
+func mongoSessionFactory() *mgo.Session {
+	var err error
+
+	dialInfo := &mgo.DialInfo{
+		Addrs:     mongo_host,
+		Direct:    false,
+		Timeout:   mongo_timeout,
+		PoolLimit: mongo_pool_limit,
+	}
+
+	mongo_session, err = mgo.DialWithInfo(dialInfo)
+
+	if err != nil {
+		LoggerError().Error("mongo connect error : %s", err.Error())
+		panic("mongo connect error")
+	} else {
+		mongo_session.SetMode(mgo.Eventual, true)
+		return mongo_session
+	}
+}
+
+func MongoSession() *mgo.Session {
+	if mongo_session == nil {
+		mongo_session = mongoSessionFactory()
+	}
+
+	return mongo_session.Copy()
 }
--- controllers/http/home.go
+++ controllers/http/home.go
@@ -6,11 +6,16 @@ import (
 	"ginder/controllers"
 	"fmt"
 	"time"
+	"gopkg.in/mgo.v2/bson"
+	"ginder/conf"
+	"gopkg.in/mgo.v2"
 )
 
 type HomeOutput struct {
-	Title string `json:"title"`
-	NowTime  string `json:"now_time"`
+	ID       bson.ObjectId `json:"id" bson:"_id"`
+	Title 	 string `json:"title" bson:"title"`
+	Username string `json:"username" bson:"username"`
+	NowTime	 string `json:"now_time" bson:"now_time"`
 }
 
 func Home(c *gin.Context) {
@@ -20,9 +25,32 @@ func Home(c *gin.Context) {
 	if userInfo.Username == "" {
 		controllers.ThrowError(c, -1, "登录超时，请重新登录")
 	} else {
+		mongoSession := conf.MongoSession()
+		defer mongoSession.Close()
+
+		con := mongoSession.DB("passport").C("home_info")
 		output := HomeOutput{}
-		output.Title = fmt.Sprintf("欢迎您，%s", userInfo.Username)
-		output.NowTime = time.Now().Format("2006-01-02 15:04:05")
+
+		if err := con.Find(bson.M{"username": userInfo.Username}).One(&output); err != nil {
+			if err.Error() != mgo.ErrNotFound.Error() {
+				conf.LoggerLogic().Error("mongo find error : %s", err.Error())
+				controllers.ThrowError(c, -1, "首页显示异常，请稍后再试")
+			} else {
+				output.ID = bson.NewObjectId()
+				output.Title = fmt.Sprintf("欢迎您，%s", userInfo.Username)
+				output.Username = userInfo.Username
+				output.NowTime = time.Now().Format("2006-01-02 15:04:05")
+
+				err := con.Insert(&output)
+
+				if err != nil {
+					conf.LoggerLogic().Error("mongo insert error : %s", err.Error())
+					controllers.ThrowError(c, -1, "首页显示异常，请稍后再试")
+				}
+			}
+		}
+
+		output.ID = ""
 		c.JSON(http.StatusOK, controllers.SwitchResponse(0, output, ""))
 	}
 }
